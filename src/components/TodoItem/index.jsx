66d8d774ebf458687ed1cc379d4abe6f41191fb0
takamasa hirano <pingyexiaoshang@gmail.com>
2024-08-01 18:00:51 +0900

    条件付きレンダー

--- src/components/TodoItem/index.jsx
+++ src/components/TodoItem/index.jsx
@@ -2,7 +2,9 @@ import PropTypes from 'prop-types'
 import styles from './index.module.css'
 
 export const TodoItem = ({ name, isToday, isCompleted }) => {
-  return <li className={styles.name}>{name}</li>
+  if (isToday) {
+    return  <li className={styles.name}>{name}{isCompleted ? '✔︎' : ''}</li>
+  }
 }
 
 TodoItem.propTypes = {
