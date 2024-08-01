90dbc9b1877547c81dfc2f488c00906b3b4de3d5
takamasa hirano <pingyexiaoshang@gmail.com>
2024-08-01 18:27:22 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { TodoItem } from './components/TodoItem'
 import { LanguageList } from './components/LanguageList'
 import { AlertButton } from './components/AlertButton'
 import { InputField } from './components/InputField'
+import { CharacterImage } from './components/CharacterImage'
 
 export const App = () => {
   return (
@@ -52,6 +53,10 @@ export const App = () => {
       <hr />
 
       <InputField />
+
+      <hr />
+
+      <CharacterImage />
     </div>
   )
 }
--- src/components/CharacterImage/index.jsx
+++ src/components/CharacterImage/index.jsx
@@ -1,12 +1,16 @@
 import axios from 'axios'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 export const CharacterImage = () => {
   const [imgUrl, setImgUrl] = useState('')
 
-  axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
-    setImgUrl(res.data.sprites.other['official-artwork']['front_default'])
-  })
+  useEffect(() => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/1').then((res) => {
+      console.log(res.data.sprites.other['official-artwork']['front_default'])
+      setImgUrl(res.data.sprites.other['official-artwork']['front_default'])
+    })
+  }, [])
+
 
   return (
     <>
