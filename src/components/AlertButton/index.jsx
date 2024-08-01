f7bf832a844d7d520da7f3ba88d6f5c9bb542c8c
takamasa hirano <pingyexiaoshang@gmail.com>
2024-08-01 18:03:27 +0900

    イベントハンドラ

--- src/components/AlertButton/index.jsx
+++ src/components/AlertButton/index.jsx
@@ -1,5 +1,8 @@
 import styles from './index.module.css'
 
 export const AlertButton = () => {
-  return <button className={styles.button}>Alert</button>
+  const handleAlert = () => {
+    alert('Alertが実行されました');
+  }
+  return <button onClick={handleAlert} className={styles.button}>Alert</button>
 }
